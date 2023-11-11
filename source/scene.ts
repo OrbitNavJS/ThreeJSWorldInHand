1e6e29e8e4d5ee51a48f802c21edc9b95e776e6e
BelanaZ <66524915+BelanaZ@users.noreply.github.com>
2023-11-11 15:10:47 +0100

    adapt lil gui parameters

--- source/scene.ts
+++ source/scene.ts
@@ -14,12 +14,9 @@ import {
   PCFSoftShadowMap,
   PerspectiveCamera,
   PlaneGeometry,
-  PointLight,
-  PointLightHelper,
   Scene,
   WebGLRenderer,
 } from 'three'
-import { DragControls } from 'three/examples/jsm/controls/DragControls'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import Stats from 'three/examples/jsm/libs/stats.module'
 import * as animations from './helpers/animations'
@@ -34,14 +31,10 @@ let renderer: WebGLRenderer
 let scene: Scene
 let loadingManager: LoadingManager
 let ambientLight: AmbientLight
-let pointLight: PointLight
-let cube: Mesh
 let cubeGroup: Group
 let camera: PerspectiveCamera
 let cameraControls: OrbitControls
-let dragControls: DragControls
 let axesHelper: AxesHelper
-let pointLightHelper: PointLightHelper
 let clock: Clock
 let stats: Stats
 let gui: GUI
@@ -84,42 +77,12 @@ function init() {
   // ===== 💡 LIGHTS =====
   {
     ambientLight = new AmbientLight('white', 5)
-    pointLight = new PointLight('#ffdca8', 1.2, 100)
-    pointLight.position.set(-2, 3, 3)
-    pointLight.castShadow = true
-    pointLight.shadow.radius = 4
-    pointLight.shadow.camera.near = 0.5
-    pointLight.shadow.camera.far = 4000
-    pointLight.shadow.mapSize.width = 2048
-    pointLight.shadow.mapSize.height = 2048
     scene.add(ambientLight)
-    scene.add(pointLight)
   }
 
   // ===== 📦 OBJECTS =====
   {
-    const sideLength = 1
-    const gridSize = 10;
-    const spacing = 1.2;
-    const minHeight = 0.2;
-    const maxHeight = 2.0;
-    const gridCenterOffset = (gridSize - 1) * spacing * 0.5;
-
-    const cubeGeometry = new BoxGeometry(sideLength, sideLength, sideLength)
-    const cubeMaterial = new MeshStandardMaterial({
-      color: '#f70893',
-      metalness: 0.5,
-      roughness: 0.7,
-    })
-    cube = new Mesh(cubeGeometry, cubeMaterial)
-    cube.castShadow = true
-    cube.position.y = 0.5
-    cube.position.set(
-      (gridSize - 1) * spacing * 0.5,
-      0.5,
-      (gridSize - 1) * spacing * 0.5
-    );
-
+    // plane
     const planeGeometry = new PlaneGeometry(15, 15)
     const planeMaterial = new MeshLambertMaterial({
       color: 'gray',
@@ -132,20 +95,23 @@ function init() {
     const plane = new Mesh(planeGeometry, planeMaterial)
     plane.rotateX(Math.PI / 2)
     plane.receiveShadow = true
-
-    scene.add(cube)
     scene.add(plane)
 
+    // cubes
+    const gridSize = 10;
+    const spacing = 1.2;
+    const gridCenterOffset = (gridSize - 1) * spacing * 0.5;
+
+    const sideLength = 1;
+    const minHeight = 0.2;
+    const maxHeight = 2.0;
+
     cubeGroup = new Group();
+    const cubeGeometry = new BoxGeometry(sideLength, sideLength, sideLength);
 
     for (let i = 0; i < gridSize; i++) {
       for (let j = 0; j < gridSize; j++) {
-        if (i == 9 && j ==9) {
-          break;
-        }
-        const sideLength = 1;
-        const cubeGeometry = new BoxGeometry(sideLength, sideLength, sideLength);
-
+        
         const randomHeight = Math.random() * (maxHeight - minHeight) + minHeight;
         const randomColor = new Color('#ff66a1').lerp(new Color('#ffffff'), Math.random());
 
@@ -164,7 +130,6 @@ function init() {
         cubeGroup.add(tmpCube);
       }
     }
-
   scene.add(cubeGroup);
   }
 
@@ -177,33 +142,6 @@ function init() {
   // ===== 🕹️ CONTROLS =====
   {
     cameraControls = new OrbitControls(camera, canvas)
-    // cameraControls.target = cube.position.clone()
-    // cameraControls.enableDamping = true
-    // cameraControls.autoRotate = false
-    // cameraControls.update()
-
-    dragControls = new DragControls([cube], camera, renderer.domElement)
-    dragControls.addEventListener('hoveron', (event) => {
-      ((event.object as THREE.Mesh).material as MeshLambertMaterial).emissive.set('orange')
-    })
-    dragControls.addEventListener('hoveroff', (event) => {
-      ((event.object as THREE.Mesh).material as MeshLambertMaterial).emissive.set('black')
-    })
-    dragControls.addEventListener('dragstart', (event) => {
-      cameraControls.enabled = false;
-      animation.play = false;
-      ((event.object as THREE.Mesh).material as MeshLambertMaterial).emissive.set('black');
-      ((event.object as THREE.Mesh).material as MeshLambertMaterial).opacity = 0.7;
-      ((event.object as THREE.Mesh).material as MeshLambertMaterial).needsUpdate = true;
-    })
-    dragControls.addEventListener('dragend', (event) => {
-      cameraControls.enabled = true;
-      animation.play = true;
-      ((event.object as THREE.Mesh).material as MeshLambertMaterial).emissive.set('black');
-      ((event.object as THREE.Mesh).material as MeshLambertMaterial).opacity = 1;
-      ((event.object as THREE.Mesh).material as MeshLambertMaterial).needsUpdate = true;
-    })
-    dragControls.enabled = false
 
     // Full screen
     window.addEventListener('dblclick', (event) => {
@@ -218,10 +156,6 @@ function init() {
     axesHelper = new AxesHelper(4)
     axesHelper.visible = false
     scene.add(axesHelper)
-
-    pointLightHelper = new PointLightHelper(pointLight, undefined, 'orange')
-    pointLightHelper.visible = false
-    scene.add(pointLightHelper)
 
     const gridHelper = new GridHelper(20, 20, 'teal', 'darkgray')
     gridHelper.position.y = -0.01
@@ -239,33 +173,29 @@ function init() {
   {
     gui = new GUI({ title: '🐞 Debug GUI', width: 300 })
 
-    const cubeOneFolder = gui.addFolder('Cube one')
+    const cubesFolder = gui.addFolder('Cubes')
 
-    cubeOneFolder.add(cube.position, 'x').min(-10).max(10).step(0.1).name('pos x')
-    cubeOneFolder.add(cube.position, 'y').min(-10).max(10).step(0.1).name('pos y')
-    cubeOneFolder.add(cube.position, 'z').min(-10).max(10).step(0.1).name('pos z')
+    cubesFolder.add(cubeGroup.position, 'x').min(-10).max(10).step(0.1).name('pos x')
+    cubesFolder.add(cubeGroup.position, 'y').min(-10).max(10).step(0.1).name('pos y')
+    cubesFolder.add(cubeGroup.position, 'z').min(-10).max(10).step(0.1).name('pos z')
 
-    cubeOneFolder.add(cube.material, 'wireframe')
-    cubeOneFolder.addColor(cube.material, 'color')
-    cubeOneFolder.add(cube.material, 'metalness', 0, 1, 0.1)
-    cubeOneFolder.add(cube.material, 'roughness', 0, 1, 0.1)
+    cubesFolder.add(cubeGroup.rotation, 'x', -Math.PI * 2, Math.PI * 2, Math.PI / 4).name('rotate x')
+    cubesFolder.add(cubeGroup.rotation, 'y', -Math.PI * 2, Math.PI * 2, Math.PI / 4).name('rotate y')
+    cubesFolder.add(cubeGroup.rotation, 'z', -Math.PI * 2, Math.PI * 2, Math.PI / 4).name('rotate z')
+    
+    cubesFolder.addColor({ color: '#ff66a1' }, 'color').onChange((color) => {
+      cubeGroup.children.forEach((cube) => {
+        (cube.material as MeshStandardMaterial).color.set(new Color(color).lerp(new Color('#ffffff'), Math.random()))
+      })
+    });
 
-    cubeOneFolder.add(cube.rotation, 'x', -Math.PI * 2, Math.PI * 2, Math.PI / 4).name('rotate x')
-    cubeOneFolder.add(cube.rotation, 'y', -Math.PI * 2, Math.PI * 2, Math.PI / 4).name('rotate y')
-    cubeOneFolder.add(cube.rotation, 'z', -Math.PI * 2, Math.PI * 2, Math.PI / 4).name('rotate z')
-
-    cubeOneFolder.add(animation, 'enabled').name('animated')
-
-    const controlsFolder = gui.addFolder('Controls')
-    controlsFolder.add(dragControls, 'enabled').name('drag controls')
-
+    cubesFolder.add(animation, 'enabled').name('animated')
+    
     const lightsFolder = gui.addFolder('Lights')
-    lightsFolder.add(pointLight, 'visible').name('point light')
     lightsFolder.add(ambientLight, 'visible').name('ambient light')
 
     const helpersFolder = gui.addFolder('Helpers')
     helpersFolder.add(axesHelper, 'visible').name('axes')
-    helpersFolder.add(pointLightHelper, 'visible').name('pointLight')
 
     const cameraFolder = gui.addFolder('Camera')
     cameraFolder.add(cameraControls, 'autoRotate')
@@ -297,8 +227,8 @@ function animate() {
   stats.update()
 
   if (animation.enabled && animation.play) {
-    animations.rotate(cube, clock, Math.PI / 3)
-    animations.bounce(cube, clock, 1, 0.5, 0.5)
+    animations.rotate(cubeGroup, clock, Math.PI / 3)
+    animations.bounce(cubeGroup, clock, 1, 0.5, 0.5)
   }
 
   if (resizeRendererToDisplaySize(renderer)) {
