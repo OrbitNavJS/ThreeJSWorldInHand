88420c50d65a27a8f5b42b0996c55a5537caa0aa
Stastez <78495486+Stastez@users.noreply.github.com>
2024-02-22 17:27:26 +0100

    Fix scripts for website

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,39 @@
 import { defineConfig } from 'vite'
 import dts from 'vite-plugin-dts'
 
-export default defineConfig({
-    base: '/ThreeJSWorldInHand/',
-    plugins: [
-        dts({
-            include: [ 'source/worldInHandControls.ts' ],
-            rollupTypes: true,
-            outDir: 'dist',
-        })
-    ],
-    build: {
-        copyPublicDir: false,
-        lib: {
-            entry: 'source/worldInHandControls.ts',
-            formats: ['es'],
-            fileName: 'three-world-in-hand-controls'
-        },
-        rollupOptions: {
-            external: ['three'],
-            output: {
-                dir: 'dist'
+export default defineConfig((configEnv) => {
+    console.log(configEnv.mode);
+
+    if (configEnv.mode === 'library') {
+        return {
+            base: '/ThreeJSWorldInHand/',
+            plugins: [
+                dts({
+                    include: [ 'source/worldInHandControls.ts' ],
+                    rollupTypes: true,
+                    outDir: 'dist',
+                })
+            ],
+            build: {
+                copyPublicDir: false,
+                lib: {
+                    entry: 'source/worldInHandControls.ts',
+                    formats: ['es'],
+                    fileName: 'three-world-in-hand-controls'
+                },
+                rollupOptions: {
+                    external: ['three'],
+                    output: {
+                        dir: 'dist'
+                    }
+                }
             }
-        }
+        };
+    } else if (configEnv.mode === 'production') {
+        return {
+            base: '/ThreeJSWorldInHand/'
+        };
+    } else {
+        return {};
     }
 })
