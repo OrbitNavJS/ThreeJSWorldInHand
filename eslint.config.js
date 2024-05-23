214df13617e76dbf6933768485da5b0ffa339ba8
stastez <78495486+Stastez@users.noreply.github.com>
2024-05-23 18:05:18 +0200

    Update deps

---
+++ eslint.config.js
@@ -0,0 +1,17 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+
+export default [
+	{languageOptions: { globals: globals.browser }},
+	pluginJs.configs.recommended,
+	...tseslint.configs.recommended,
+	{
+		"rules": {
+			"@typescript-eslint/semi": "error",
+			"@typescript-eslint/array-type": ["error", {"default": "generic"}],
+			"@typescript-eslint/consistent-type-assertions": "error"
+		}
+	}
+];
